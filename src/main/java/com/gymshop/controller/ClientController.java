3aea3edfef7e22d7a1a71991904fff991b3426d8
tienkh216 <tienpve1@gmail.com>
2021-11-05 21:47:12 +0700

    update controller

--- src/main/java/com/gymshop/controller/ClientController.java
+++ src/main/java/com/gymshop/controller/ClientController.java
@@ -11,4 +11,24 @@ public class ClientController {
 		return ("/client/home");
 	}
 	
+	@RequestMapping("/client/product")
+	public String clientProduct(Model model) {
+		return ("/client/products");
+	}
+	
+	@RequestMapping("/client/cart")
+	public String shoppingCart(Model model) {
+		return ("/client/shoppingCart");
+	}
+	
+	@RequestMapping("/client/checkout")
+	public String clientCheckout(Model model) {
+		return ("/client/checkout");
+	}
+	
+	@RequestMapping("/client/productDetail")
+	public String productDetail(Model model) {
+		return ("/client/productDetail");
+	}
+	
 }
--- src/main/java/com/gymshop/controller/admin/HomeController.java
+++ src/main/java/com/gymshop/controller/admin/HomeController.java
@@ -10,4 +10,19 @@ public class HomeController {
 	public String homePage(Model model) {
 		return ("/admin/index");
 	}
+	
+	@RequestMapping("/admin/product")
+	public String adminProduct(Model model) {
+		return ("/admin/products");
+	}
+	
+	@RequestMapping("/admin/order")
+	public String adminOrder(Model model) {
+		return ("/admin/orders");
+	}
+	
+	@RequestMapping("/admin/category")
+	public String adminCategory(Model model) {
+		return ("/admin/categories");
+	}
 }
