1034659729b71b0810ac28f0d1eda7febbcd974d
tienkh216 <tienpve1@gmail.com>
2021-11-14 19:08:22 +0700

    update

--- src/main/java/com/gymshop/controller/admin/HomeController.java
+++ src/main/java/com/gymshop/controller/admin/HomeController.java
@@ -3,19 +3,18 @@ package com.gymshop.controller.admin;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import groovyjarjarpicocli.CommandLine.Model;
+
 @Controller
 public class HomeController {
 	@RequestMapping("/admin/home")
 	public String admin() {
 		return "redirect:/admin/index.html";
 	}
-<<<<<<< HEAD
+
 	@RequestMapping("/products")
 	public String products(Model model) {
 		return ("/admin/products");
 	}
-=======
-	
-	
->>>>>>> master
+
 }
