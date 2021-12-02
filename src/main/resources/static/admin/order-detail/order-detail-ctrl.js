f1898f7eaaff60ef567bcb7bc6acc0bcfb7cce52
ha <some@email.com>
2021-12-02 21:54:22 +0700

    Phuc

--- src/main/resources/static/admin/order-detail/order-detail-ctrl.js
+++ src/main/resources/static/admin/order-detail/order-detail-ctrl.js
@@ -4,17 +4,19 @@ app.controller("order-detail-ctrl", function($scope, $http, $rootScope, $routePa
     $scope.items =[];
     $scope.username;
     $scope.address;
+    $scope.paymethod;
     $scope.total=0;
     $scope.initialize = function(){
         $http.get(`/rest/orderdetail/${id}`).then(resp=>{
-            console.log(resp.data);
             $scope.items = resp.data;
             $scope.username = resp.data[0].order.account.username;
             $scope.address = resp.data[0].order.address;
+            $scope.paymethod = resp.data[0].order.paymentMethod.description;
+            console.log( $scope.paymethod);
             for(var i =0; i<$scope.items.length;i++){
                 $scope.total+= (resp.data[i].product.price) * (resp.data[i].quantity);
             }
-            console.log($scope.total);
+            
         })
     }
    
