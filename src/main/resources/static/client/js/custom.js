17c304d490bcb90ba1f127f337ccec8373d43b0c
phamtrieuduyminh1 <phamtrieuduyminh1@gmail.com>
2021-12-07 00:58:13 +0700

    tiến béo cay

--- src/main/resources/static/client/js/custom.js
+++ src/main/resources/static/client/js/custom.js
@@ -487,3 +487,21 @@ function searchToggle(obj, evt) {
 		container.find('.search-input').val('');
 	}
 }
+
+//
+// jQuery(document).ready(($) => {
+// 	$('.quantity').on('click', '.plus', function(e) {
+// 		let $input = $(this).prev('input.qty');
+// 		let val = parseInt($input.val());
+// 		$input.val( val+1 ).change();
+// 	});
+//
+// 	$('.quantity').on('click', '.minus',
+// 		function(e) {
+// 			let $input = $(this).next('input.qty');
+// 			var val = parseInt($input.val());
+// 			if (val > 0) {
+// 				$input.val( val-1 ).change();
+// 			}
+// 		});
+// });
