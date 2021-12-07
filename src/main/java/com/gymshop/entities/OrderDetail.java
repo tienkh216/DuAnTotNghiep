12d44ea6b45247401fd0fa6f1dab647d6979fdd1
tienkh216 <tienpve1@gmail.com>
2021-12-08 00:56:47 +0700

    tiên làm hết OrderListCreate.java

--- src/main/java/com/gymshop/entities/OrderDetail.java
+++ src/main/java/com/gymshop/entities/OrderDetail.java
@@ -19,7 +19,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor 
 @NoArgsConstructor
 @Entity 
-@Table(name = "OrderDetails")
+@Table(name = "order_details")
 public class OrderDetail implements Serializable  {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
